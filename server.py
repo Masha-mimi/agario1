256d1d3ac46772dd390c0120f1a767606d140e44
Masha-mimi <u8554689@gmail.com>
2026-06-13 17:30:41 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -27,7 +27,9 @@ def handle_data():
                     if len(parts) == 5:
                         pid, x, y, r = map(int, parts[:4])
                         name = parts[-1]
-                        players[conn] = {"id": pid, "x": x, "y": y, "r": r, "name": name}
+                        players[conn] = {"id": pid, "x": x,
+                                         "y": y, "r": r,
+                                         "name": name}
                         player_data[conn] = players[conn]
             except:
                 continue
@@ -39,41 +41,44 @@ def handle_data():
             for conn2 in player_data:
                 if conn1 == conn2 or conn2 in eliminated: continue
                 p2 = player_data[conn2]
-                dx, dy = p1["x"] -p2["x"], p1["y"] -p2["y"]
+                dx, dy = p1["x"] - p2["x"], p1["y"] - p2["y"]
                 distance = (dx ** 2 + dy ** 2) ** 0.5
-                if distance < p1["r"] + p2["r"] and p1["r"] > p2["r"] * 1.1:
+                if (distance < p1["r"] + p2["r"]
+                        and p1["r"] > p2["r"] * 1.1):
                     p1["r"] += int(p2["r"] * 0.5)
                     players[conn1] = p1
                     eliminated.append(conn2)
-                    for conn in list(players.keys()):
-                        if conn in eliminated:
-                            try:
-                                conn.send("LOSE".encode())
-                            except:
-                                pass
-                            to_remove.append(conn)
-                            continue
 
-                        try:
-                            packet = "|".join([f"{p['id']},{p['x']},{p['y']},{p['r']},{p['name']}" for c, p in players.items() if c != conn and c not in eliminated]) + "|"
-                            conn.send(packet.encode())
-                        except:
-                            to_remove.append(conn)
+        for conn in list(players.keys()):
+            if conn in eliminated:
+                try:
+                    conn.send("LOSE".encode())
+                except:
+                    pass
+                to_remove.append(conn)
+                continue
 
-                    for conn in to_remove:
-                        players.pop(conn, None)
-                        conn_ids.pop(conn, None)
+            try:
+                packet = "|".join([f"{p['id']},{p['x']},{p['y']},{p['r']},{p['name']}" for c, p in players.items() if c != conn and c not in eliminated]) + "|"
+                conn.send(packet.encode())
+            except:
+                to_remove.append(conn)
 
-                Thread(target=handle_data, daemon=True).start()
-                print("SERVER running...")
+        for conn in to_remove:
+            players.pop(conn, None)
+            conn_ids.pop(conn, None)
 
-                while True:
-                    try:
-                        conn, addr = sock.accept()
-                        conn.setblocking(False)
-                        id_counter += 1
-                        players[conn] = {"id": id_counter, "x": 0, "y": 0, "r": 20, "name": None}
-                        conn_ids[conn] = id_counter
-                        conn.send(f"{id_counter},0,0,20".encode())
-                    except:
-                        pass
+
+Thread(target=handle_data, daemon=True).start()
+print("SERVER running...")
+
+while True:
+    try:
+        conn, addr = sock.accept()
+        conn.setblocking(False)
+        id_counter += 1
+        players[conn] = {"id": id_counter, "x": 0, "y": 0, "r": 20, "name": None}
+        conn_ids[conn] = id_counter
+        conn.send(f"{id_counter},0,0,20".encode())
+    except:
+        pass
